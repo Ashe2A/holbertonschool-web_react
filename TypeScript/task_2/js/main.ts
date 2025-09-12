8db3e0c7d37049bee422365ae52e50cd4a97c21b
Ashe2A <supersonicx001@gmail.com>
2025-09-12 22:52:35 +0200

    TypeScript (6) - Creating functions specific to employees

--- TypeScript/task_2/js/main.ts
+++ TypeScript/task_2/js/main.ts
@@ -22,29 +22,28 @@ class Director implements DirectorInterface {
 
   workDirectorTasks(): string {
     return 'Getting to director tasks';
-  };
-};
+  }
+}
 
 class Teacher implements TeacherInterface {
   workFromHome(): string {
     return 'Cannot work from home';
-  };
+  }
 
   getCoffeeBreak(): string {
     return 'Cannot have a break';
-  };
+  }
 
   workTeacherTasks(): string {
     return 'Getting to work';
-  };
-};
+  }
+}
 
 function createEmployee(salary: number | string): Director | Teacher {
   if ((typeof salary === 'number') && salary < 500) {
     return new Teacher();
-  } else {
-    return new Director();
   }
+  return new Director();
 }
 
 console.log(createEmployee(200));
@@ -53,3 +52,19 @@ console.log(createEmployee(1000));
 // Director
 console.log(createEmployee('$500'));
 // Director
+
+function isDirector(employee: Director | Teacher): employee is Director {
+  return employee instanceof Director;
+}
+
+function executeWork(employee: Director | Teacher): string {
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks();
+  }
+  return employee.workTeacherTasks();
+}
+
+executeWork(createEmployee(200));
+// Getting to work
+executeWork(createEmployee(1000));
+// Getting to director tasks
